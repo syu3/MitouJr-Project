fd507c145e2aaab5567ec8b8e6dd8b5e7d240a1d
syu3 <syusan3@icloud.com>
2017-07-04 19:17:27 +0900

    7/4
    ajax通信

--- fff.php
+++ fff.php
@@ -1,1 +1,9 @@
-<?php echo $_POST['name']; ?>
+<!-- echo $_POST['name']; -->
+<?php
+if(isset($_POST["name"]) && preg_match("http://typingcode.16mb.com/index.php"){
+
+    echo file_get_contents($_POST["name"]);
+}else{
+    echo "error";
+}
+?>
